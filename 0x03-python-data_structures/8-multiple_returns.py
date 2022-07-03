76788c1f3c1a59467a424cdde8fadb903d6c5997
kalu_coder <awesomecoding51@gmail.com>
2022-07-03 21:10:59 +0300

    multiple value returner

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+def multiple_returns(sentence):
+    str_len = len(sentence)
+    if sentence == "":
+        first_char = "None"
+    else:
+        first_char = sentence[0]
+    return (str_len, first_char)
