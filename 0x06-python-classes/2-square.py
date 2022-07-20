8ea373deb6a39c7e2a60a18db5debf940658301d
kalu_coder <awesomecoding51@gmail.com>
2022-07-20 09:28:56 +0300

    code debugged

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -14,8 +14,9 @@ def __init__(self, size=0):
     """
 
     def __init__(self, size):
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        if type(size) is not int:
-            raize TypeError("size must be an integer")
-        self.__size = size
+        if isinstance(size, int):
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = size
+        else:
+            raise TypeError("size must be an integer")
