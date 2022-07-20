fa47589c012f897ba59440d6da1ba36898dce9cb
kalu_coder <awesomecoding51@gmail.com>
2022-07-20 08:03:39 +0300

    initial commit to empty class creater module

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+File: 0-square.py
+Desc: This module contains a single empty
+class definition for class called square
+Author: Kaleab Shiferaw Girma
+Date Created: Jul 20 2022
+"""
+
+
+class Square():
+    """
+    an empty class created.
+    """
+    pass
