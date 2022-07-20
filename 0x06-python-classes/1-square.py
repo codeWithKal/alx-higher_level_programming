4aa9bbf6a54243dff04b692bc54360b6bb77f644
kalu_coder <awesomecoding51@gmail.com>
2022-07-20 08:24:53 +0300

    initial commit to a class that can be initialized with size attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+File: 1-square.py
+description: a module to define a square with initialization of size
+Author: Kaleab Shiferaw Girma
+Date: July 20 2022
+"""
+
+
+class Square():
+    """
+    This class can be initialized to an square object with its own 'size'
+    """
+    def __init__(self, size):
+        self.size = size
