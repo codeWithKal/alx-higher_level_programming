09ceb9b13f7c7dcbf65cda31292fb47f6c63bbb5
kalu_coder <awesomecoding51@gmail.com>
2022-07-22 17:41:13 +0300

    code debugged

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -30,7 +30,7 @@ class Square():
         if isinstance(value, int):
             if value < 0:
                 raise ValueError("size must be >= 0")
-            self.__size = size
+            self.__size = value
         else:
             raise TypeError("size must be an integer")
 
