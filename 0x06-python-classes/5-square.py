ca0bae0b39dbc0f7a44cc303169ff861aa2ecd5f
codeWithKal <awesomecoding51@gmail.com>
2022-07-22 17:31:36 +0300

    initial commit to sixth task

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+File: 5-square.py
+Description: addition to 4-square.py and prints the square
+Author: kaleab shiferaw girma
+Date: july 22 2022
+"""
+class Square():
+    """
+    this class contains constructor, getter methods and setters
+    in addition to square painter
+    """
+
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        getter method
+        """
+        return(self.__size)
+    @self.setter
+    def size(self, value):
+        if isinstance(value, int):
+            if value < 0:
+                raise ValueError("size must be >=0")
+            self.__size = value
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """
+        A method that returns the current square area
+        """
+
+
+        return (self.__size ** 2)
+    def my_print(self):
+        """
+        prints the square:
+        """
+
+
+        for i in range(0, size):
+            for j in range(0, size):
+                print("#", end=" ")
+            print()
