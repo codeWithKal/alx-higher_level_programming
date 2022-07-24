326deacd96ae3ee5db9fbf1f35ac8475a45c4bf8
kalu_coder <awesomecoding51@gmail.com>
2022-07-24 08:07:21 +0300

    code bug hunted

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -42,10 +42,10 @@ class Square():
         prints the square:
         """
         if self.__size == 0:
-            print()
+            print("")
 
         else:
             for i in range(0, self.__size):
                 for j in range(0, self.__size):
                     print("#", end=" ")
-                    print()
+                print("")
