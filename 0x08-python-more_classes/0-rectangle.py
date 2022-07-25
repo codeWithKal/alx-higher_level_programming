3133ecb8407dc34c1728fe85ffb61bdd3418d2f6
kalu_coder <awesomecoding51@gmail.com>
2022-07-25 20:07:45 +0300

    initial commit to the module having empty class

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+File: 0-rectangle.py
+Description: a module containing an empty class for Rectangle
+Author: Kaleab Shiferaw Girma
+Date: July 15 2022
+"""
+
+
+class Rectangle():
+    """
+    an empty class for defining a rectangle.
+    """
+    def __init__(self):
+        """
+        initializer method
+        """
