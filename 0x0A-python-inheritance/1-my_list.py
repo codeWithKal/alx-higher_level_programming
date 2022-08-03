9c5a667d2f4f067bf3931424e43b3797162ce886
kalu_coder <awesomecoding51@gmail.com>
2022-08-03 15:29:27 +0300

    list order and printer initially commited with its test file

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+File: 1-my_list.py
+Description: a module that contains a superclass and its child class
+Author: kaleab Shiferaw Girma(kalu-coder)
+Date: 1st August
+"""
+
+
+class MyList(list):
+    """
+    a class that inherits from a list superclass
+    """
+    def print_sorted(self):
+        """
+        this method prints a sorted version of a list passed to it
+        """
+        print(sorted(self))
