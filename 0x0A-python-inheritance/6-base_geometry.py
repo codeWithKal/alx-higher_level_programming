4977b550f7f645d10de964b3970e1497a7a74ed5
kalu_coder <awesomecoding51@gmail.com>
2022-08-03 17:03:16 +0300

    initial commit to task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+File: 6-base_geometry.py
+Desc: a module containing a class that throws an exception.
+Author: kaleab Shiferaw girma
+Date 3rd Aug 2022
+"""
+
+
+class BaseGeometry():
+    """
+    an empty class
+    """
+    def __init__(self):
+        """
+        an intilizer method
+        """
+    def area(self):
+        """
+         a method that throws an exception
+        """
+        raise Exception("area() is not implemented")
