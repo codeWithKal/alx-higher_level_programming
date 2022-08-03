33add7bb6a0cc4a5f234d389fbbe9ab3ebadef7f
kalu_coder <awesomecoding51@gmail.com>
2022-08-03 16:55:29 +0300

    initial commit to task 5

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+File: 5-base_geometry.py
+Description: a module containing empty class
+Author: Kaleab Shiferaw Girma
+Date: 3rd August 2022
+"""
+
+
+class BaseGeometry():
+    """
+    an empty class BaseGeometry
+    """
+    def __init__(self):
+        """
+        initializer method
+        """
