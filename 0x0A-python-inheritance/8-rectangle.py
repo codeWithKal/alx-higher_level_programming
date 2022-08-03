3cc0d86ba65090a1f3ccad6a58baca76aba912d0
kalu_coder <awesomecoding51@gmail.com>
2022-08-03 18:37:51 +0300

    initial commit to task 8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+from 7-base_geometry import BaseGeometry
+
+"""
+File: 8-rectangle.py
+Desc: a module containing a class Rectangle that inherits.
+Author: Kaleab Shiferaw Girma
+Date: 3rd August 2022
+"""
+
+
+class Rectangle(BaseGeometry):
+    """
+    Rectangle class inherited from BaseGeometry
+    """
+    def __init__(self, width, height):
+        """
+        Instantializer method
+
+        Args:
+            width: width of a rectangle
+            heigh: height of a rectangle
+        """
+        super().__init__()
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
