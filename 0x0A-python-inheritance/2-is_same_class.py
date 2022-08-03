e6e7fb54c7e6b31505efa5c4181629405a2267b4
kalu_coder <awesomecoding51@gmail.com>
2022-08-03 16:15:49 +0300

    inital commit to task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+File: 2-is_same_class.py
+Description: a module containing a method checkes wheather
+        a class is an instance of a_class or not
+Author: Kaleab Shiferaw Girma
+Date: August 3 2022
+"""
+def is_same_class(obj, a_class):
+    """
+    checkes if a given object is an instance of a class
+    
+    Args:
+        obj(any): an object to be checked
+        a_class(any): a class to be checked against
+    """
+
+    if isinstance(obj, a_class):
+        return True
+    return False
