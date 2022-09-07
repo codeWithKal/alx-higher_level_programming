8b3b0d8b820ba9a504b883b50e51673292d94c22
kalu_coder <awesomecoding51@gmail.com>
2022-09-07 19:11:00 +0300

    multiple record entered

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,13 @@
+-- A table creater with  records
+CREATE TABLE IF NOT EXISTS second_table(
+	id INT,
+	name VARCHAR(256),
+	score INT
+	);
+-- Multiple records are added
+INSERT INTO second_table(id, name, score) VALUES
+	(1, "John", 10),
+	(2, "Alex", 3),
+	(3, "Bob", 14),
+	(4, "George", 8)
+	;
