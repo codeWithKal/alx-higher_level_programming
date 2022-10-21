3ab3531b426567c16e73735e2d9e6240152d89e8
kalu_coder <awesomecoding51@gmail.com>
2022-10-21 19:34:32 +0300

    edited

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     if len(sys.argv) > 2:
         url = sys.argv[1]
         email = sys.argv[2]
-        data = byte(parse.urlencode([('email', email)]), 'utf-8')
+        data = bytes(parse.urlencode([('email', email)]), 'utf-8')
         with request.urlopen(url, data=data) as response:
             print(response.read().decode('utf-8'))
 
