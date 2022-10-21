b435fe42c1dcd3b64eac7b0731ac2c0117d4ab99
kalu_coder <awesomecoding51@gmail.com>
2022-10-21 20:29:46 +0300

    intial commit to the script that uses requests module

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+file: 4-hbtn_status.py
+Description: A script that uses requests module to fetch a website
+Author: Kaleab Shiferaw Girma
+"""
+import requests
+
+
+if __name__ == "__main__":
+    response = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
