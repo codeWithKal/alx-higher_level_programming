fa09e444e75c401ff7592ff276909647e9eb4006
kalu_coder <awesomecoding51@gmail.com>
2022-10-21 20:00:15 +0300

    third task solved

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+File: 3-error_code.py
+Description: a script that handles an HttpError
+Author: Kaleab shiferaw Girma
+"""
+from urllib import request
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        url = sys.argv[1]
+        try:
+            with urllib.request.urlopen(url) as data:
+                print(data.read().decode('utf-8'))
+        except error.HTTPError as e:
+            print("Error code: {}".format(e.code()))
