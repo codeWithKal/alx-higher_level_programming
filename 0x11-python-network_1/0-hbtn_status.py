3cef83c600c8a79c6091aeb7216feccb0cd6f2b6
kalu_coder <awesomecoding51@gmail.com>
2022-10-21 16:06:58 +0300

    string formatting is fixed as specified on the intranet

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -13,6 +13,6 @@ if __name__ == "__main__":
         if response.readable():
             data = response.read()
             print("Body response:")
-            print("\t - type: {}".format(type(data)))
-            print("\t -content: {}".format(data))
-            print("\t -utf8 content: {}".format(data.decode("utf=8")))
+            print("\t- type: {}".format(type(data)))
+            print("\t- content: {}".format(data))
+            print("\t- utf8 content: {}".format(data.decode("utf=8")))
