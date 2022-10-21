91613bc2f7cce38d38272ef2c0bf7ddfaa239ccd
kalu_coder <awesomecoding51@gmail.com>
2022-10-21 16:03:33 +0300

    pycodestyle checked

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,10 +8,11 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+    mysite = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(mysite) as response:
         if response.readable():
             data = response.read()
-            print ("Body response:")
-            print ("\t - type: {}".format(type(data)))
-            print ("\t -content: {}".format(data))
-            print ("\t -utf8 content: {}".format(data.decode("utf=8")))
+            print("Body response:")
+            print("\t - type: {}".format(type(data)))
+            print("\t -content: {}".format(data))
+            print("\t -utf8 content: {}".format(data.decode("utf=8")))
